b75e3d955102b2d98bd03a6f3e3d3332bdbf0a3b
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-13 15:55:12 +0900

    [DEV] swprintf를 대체하기 위한 문자열 포멧팅 함수 추가

--- Game/Public/Text.hpp
+++ Game/Public/Text.hpp
@@ -29,3 +29,25 @@ inline std::string Format(const std::string& Text, Args ... Argument)
 
 	return std::string(Buffer.get(), Buffer.get() + Size - 1);
 }
+
+
+/**
+ * @brief 형식화된 문자열을 반환합니다. 이 메서드는 swprintf를 대체하기 위한 메서드입니다.
+ *
+ * @see https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+ *
+ * @param Text 형식 문자열입니다.
+ * @param ... Argument 포멧팅을 수행할 데이터를 지정하는 인수입니다.
+ *
+ * @return 포멧팅이 완료된 문자열을 반환합니다.
+ */
+template<typename ... Args>
+inline std::wstring Format(const std::wstring& Text, Args ... Argument)
+{
+	size_t Size = static_cast<size_t>(std::swprintf(nullptr, 0, Text.c_str(), Argument ...)) + 1;
+
+	auto Buffer = std::make_unique<wchar_t[]>(Size);
+	std::swprintf(Buffer.get(), Size, Text.c_str(), Argument ...);
+
+	return std::wstring(Buffer.get(), Buffer.get() + Size - 1);
+}
