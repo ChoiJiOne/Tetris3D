ecbc7494481f54c3bf24a5da278fbb95a60cc2c6
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-13 16:07:58 +0900

    [DEV] C/C++ 표준라이브러리의 printf와 유사하게 파라미터 이름 변경

--- Game/Public/Text.hpp
+++ Game/Public/Text.hpp
@@ -14,18 +14,18 @@
  *
  * @see https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
  *
- * @param Text 형식 문자열입니다.
+ * @param Format 형식 문자열입니다.
  * @param ... Argument 포멧팅을 수행할 데이터를 지정하는 인수입니다.
  *
  * @return 포멧팅이 완료된 문자열을 반환합니다.
  */
 template<typename ... Args>
-inline std::string StringFormat(const std::string& Text, Args ... Argument)
+inline std::string StringFormat(const std::string& Format, Args ... Argument)
 {
-	size_t Size = static_cast<size_t>(std::snprintf(nullptr, 0, Text.c_str(), Argument ...)) + 1;
+	size_t Size = static_cast<size_t>(std::snprintf(nullptr, 0, Format.c_str(), Argument ...)) + 1;
 
 	auto Buffer = std::make_unique<char[]>(Size);
-	std::snprintf(Buffer.get(), Size, Text.c_str(), Argument ...);
+	std::snprintf(Buffer.get(), Size, Format.c_str(), Argument ...);
 
 	return std::string(Buffer.get(), Buffer.get() + Size - 1);
 }
@@ -36,18 +36,18 @@ inline std::string StringFormat(const std::string& Text, Args ... Argument)
  *
  * @see https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
  *
- * @param Text 형식 문자열입니다.
+ * @param Format 형식 문자열입니다.
  * @param ... Argument 포멧팅을 수행할 데이터를 지정하는 인수입니다.
  *
  * @return 포멧팅이 완료된 문자열을 반환합니다.
  */
 template<typename ... Args>
-inline std::wstring StringFormat(const std::wstring& Text, Args ... Argument)
+inline std::wstring StringFormat(const std::wstring& Format, Args ... Argument)
 {
-	size_t Size = static_cast<size_t>(std::swprintf(nullptr, 0, Text.c_str(), Argument ...)) + 1;
+	size_t Size = static_cast<size_t>(std::swprintf(nullptr, 0, Format.c_str(), Argument ...)) + 1;
 
 	auto Buffer = std::make_unique<wchar_t[]>(Size);
-	std::swprintf(Buffer.get(), Size, Text.c_str(), Argument ...);
+	std::swprintf(Buffer.get(), Size, Format.c_str(), Argument ...);
 
 	return std::wstring(Buffer.get(), Buffer.get() + Size - 1);
 }
