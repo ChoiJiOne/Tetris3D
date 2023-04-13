b0b3ff8cc91280e54b6bafbc526ba27011fdc5b2
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-13 15:54:17 +0900

    [DEV] snprintf를 대체하기 위한 문자열 포멧팅 함수 추가

---
+++ Game/Public/Text.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <windows.h>
+
+#include "Macro.h"
+
+
+/**
+ * @brief 형식화된 문자열을 반환합니다. 이 메서드는 snprintf를 대체하기 위한 메서드입니다.
+ *
+ * @see https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+ *
+ * @param Text 형식 문자열입니다.
+ * @param ... Argument 포멧팅을 수행할 데이터를 지정하는 인수입니다.
+ *
+ * @return 포멧팅이 완료된 문자열을 반환합니다.
+ */
+template<typename ... Args>
+inline std::string Format(const std::string& Text, Args ... Argument)
+{
+	size_t Size = static_cast<size_t>(std::snprintf(nullptr, 0, Text.c_str(), Argument ...)) + 1;
+
+	auto Buffer = std::make_unique<char[]>(Size);
+	std::snprintf(Buffer.get(), Size, Text.c_str(), Argument ...);
+
+	return std::string(Buffer.get(), Buffer.get() + Size - 1);
+}
