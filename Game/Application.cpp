a364148ca60c43358a6c94f1130fbe5428e1fd36
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-13 15:18:23 +0900

    [DEV] 애플리케이션이 시작되는 부분에 메모리 누수를 감지하는 기능 추가

--- Game/Application.cpp
+++ Game/Application.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#define _CRTDBG_MAP_ALLOC
 
-int main(void)
+#include <crtdbg.h>
+#include <cstdint>
+
+
+/**
+ * @brief 애플리케이션의 진입점입니다.
+ *
+ * @param Argc 명령행 인자의 수입니다.
+ * @param Argv 명령행 인자입니다.
+ *
+ * @return 다른 프로그램에 전달할 수 있는 상태 코드를 반환합니다.
+ */
+int32_t main(int32_t Argc, char** Argv)
 {
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	return 0;
 }
