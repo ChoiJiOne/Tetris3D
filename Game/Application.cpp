7215472e7f1f978318126606eb691c1c858458a4
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-13 15:34:44 +0900

    [DEV] 테트리스 3D 게임의 초기화/실행/종료를 수행하는 클래스 선언 및 빈 메서드 추가

--- Game/Application.cpp
+++ Game/Application.cpp
@@ -3,6 +3,68 @@
 #include <crtdbg.h>
 #include <cstdint>
 
+#include "Macro.h"
+
+
+/**
+ * @brief Tetris3D 게임을 초기화 및 실행합니다.
+ */
+class Tetris3D
+{
+public:
+	/**
+	 * @brief Tetris3D 게임을 초기화 및 실행하는 클래스의 디폴트 생성자입니다.
+	 * 
+	 * @note 이 클래스를 초기화하기 위해서는 Setup 메서드를 호출해야 합니다.
+	 */
+	Tetris3D() = default;
+
+
+	/**
+	 * @brief Tetris3D 게임을 초기화 및 실행하는 클래스의 가상 생성자입니다.
+	 * 
+	 * @note 이 클래스에서 사용한 리소스를 정리하기 위해서는 Cleanup 메서드를 호출해야 합니다.
+	 */
+	virtual ~Tetris3D() {}
+
+
+	/**
+	 * @brief Tetris3D 클래스의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Tetris3D);
+
+
+	/**
+	 * @brief Tetris3D 게임을 초기화합니다.
+	 * 
+	 * @throws Tetris3D 게임 초기화에 실패하면 표준 예외를 던집니다.
+	 */
+	void Setup()
+	{
+
+	}
+
+
+	/**
+	 * @brief Tetris3D 게임을 실행합니다.
+	 * 
+	 * @throws Tetris3D 게임 실행에 실패하면 표준 예외를 던집니다. 
+	 */
+	void Run()
+	{
+
+	}
+
+
+	/**
+	 * @brief Tetris3D 게임 내의 리소스를 정리합니다.
+	 */
+	void Cleanup()
+	{
+
+	}
+};
+
 
 /**
  * @brief 애플리케이션의 진입점입니다.
