0f47213b35a05919fa735781be56d7beaeb4ebfa
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-04-17 17:34:10 +0900

    [DEV] 햇갈릴 수 있는 주석문 수정

--- Game/Public/Window.h
+++ Game/Public/Window.h
@@ -35,12 +35,12 @@ enum class EWindowFlags : uint32_t
 
 
 /**
- * @brief 윈도우 옵션 플래그의 or 연산을 수행합니다.
+ * @brief 윈도우 옵션 플래그의 OR(|) 연산을 수행합니다.
  *
- * @param lhs or 연산의 우측 피연산자입니다.
- * @param rhs or 연산의 좌측 피연산자입니다.
+ * @param lhs OR(|) 연산의 우측 피연산자입니다.
+ * @param rhs OR(|) 연산의 좌측 피연산자입니다.
  *
- * @return or 연산을 수행한 결과를 반환합니다.
+ * @return OR(|) 연산을 수행한 결과를 반환합니다.
  */
 inline EWindowFlags operator|(const EWindowFlags& lhs, const EWindowFlags& rhs)
 {
