6f24851334ed1c7f9ee88df46cf18792370bde62
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-04-22 21:55:11 +0900

    [DEV] 윈도우 창을 생성 및 관리하는 기능 추가

---
+++ Game/Private/Window.cpp
@@ -0,0 +1,73 @@
+// @third party code - BEGIN
+#include <SDL2/SDL.h>
+// @third party code - END
+
+#include "Window.h"
+
+Window::Window(const ConstructorParam& constructorParam)
+	: Window(constructorParam.title, constructorParam.x, constructorParam.y, constructorParam.w, constructorParam.h, constructorParam.flags)
+{
+}
+
+Window::Window(const std::string& title, int32_t x, int32_t y, int32_t width, int32_t height, const EWindowFlags& flags)
+{
+	window_ = SDL_CreateWindow(title.c_str(), x, y, width, height, static_cast<uint32_t>(flags));
+	CHECK((window_ != nullptr), "failed to create window...");
+}
+
+Window::~Window()
+{
+	if (window_)
+	{
+		SDL_DestroyWindow(window_);
+		window_ = nullptr;
+	}
+}
+
+void Window::SetPosition(const int32_t& x, const int32_t& y)
+{
+	SDL_SetWindowPosition(window_, x, y);
+}
+
+void Window::GetPosition(int32_t& outX, int32_t& outY)
+{
+	SDL_GetWindowPosition(window_, &outX, &outY);
+}
+
+void Window::SetSize(const int32_t& width, const int32_t& height)
+{
+	SDL_SetWindowSize(window_, width, height);
+}
+
+void Window::GetSize(int32_t& outWidth, int32_t& outHeight)
+{
+	SDL_GetWindowSize(window_, &outWidth, &outHeight);
+}
+
+void Window::SetTitle(const std::string& title)
+{
+	SDL_SetWindowTitle(window_, title.c_str());
+}
+
+std::string Window::GetTitle()
+{
+	return std::string(SDL_GetWindowTitle(window_));
+}
+
+void Window::Show()
+{
+	SDL_ShowWindow(window_);
+}
+
+void Window::Hide()
+{
+	SDL_HideWindow(window_);
+}
+
+float Window::GetAspectRatio()
+{
+	int32_t width, height;
+	GetSize(width, height);
+
+	return static_cast<float>(width) / static_cast<float>(height);
+}
--- Game/Public/Window.h
+++ Game/Public/Window.h
@@ -46,3 +46,174 @@ inline EWindowFlags operator|(const EWindowFlags& lhs, const EWindowFlags& rhs)
 {
 	return static_cast<EWindowFlags>(static_cast<uint32_t>(lhs) | static_cast<uint32_t>(rhs));
 }
+
+
+/**
+ * @brief 윈도우 창을 생성하고 관리합니다.
+ */
+class Window
+{
+public:
+	/**
+	 * @brief 윈도우 창을 생성하고 관리하는 클래스 생성자 파라미터의 구조체입니다.
+	 */
+	struct ConstructorParam
+	{
+		const std::string& title; /** 윈도우 타이틀입니다. */
+		int32_t x;                /** 윈도우의 X좌표입니다. */
+		int32_t y;                /** 윈도우의 Y좌표입니다. */
+		int32_t w;                /** 윈도우의 가로 크기입니다. */
+		int32_t h;                /** 윈도우의 세로 크기입니다. */
+		EWindowFlags flags;       /** 윈도우의 생성 옵션입니다. */
+	};
+
+
+	/**
+	 * @brief 윈도우 창을 생성하고 관리하는 클래스의 생성자입니다.
+	 *
+	 * @param constructorParam 윈도우 클래스의 생성 파라미터입니다.
+	 *
+	 * @throws 윈도우 생성에 실패하면 C++ 표준 예외를 던집니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_CreateWindow
+	 */
+	explicit Window(const ConstructorParam& constructorParam);
+
+
+	/**
+	 * @brief 윈도우 클래스의 생성자입니다.
+	 *
+	 * @param title 윈도우의 타이틀입니다.
+	 * @param x 윈도우 왼쪽 상단의 X좌표입니다.
+	 * @param y 윈도우 왼쪽 상단의 Y좌표입니다.
+	 * @param width 윈도우의 가로 크기입니다.
+	 * @param height 윈도우의 세로 크기입니다.
+	 * @param flags 윈도우 옵션입니다.
+	 *
+	 * @throws 윈도우 생성에 실패하면 C++ 표준 예외를 던집니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_CreateWindow
+	 */
+	explicit Window(const std::string& title, int32_t x, int32_t y, int32_t width, int32_t height, const EWindowFlags& flags);
+
+
+	/**
+	 * @brief 윈도우 클래스의 가상 소멸자입니다
+	 */
+	virtual ~Window();
+
+
+	/**
+	 * @brief 윈도우 클래스의 복사 생성자와 대입 연산자를 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Window);
+
+
+	/**
+	 * @brief 게임 윈도우 위치를 변경합니다.
+	 * 
+	 * @note 이때, 단위는 픽셀단위입니다.
+	 *
+	 * @param x 스크린 상의 게임 윈도우 왼쪽 상단의 X좌표입니다.
+	 * @param y 스크린 상의 게임 윈도우 왼쪽 상단의 Y좌표입니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_SetWindowPosition
+	 */
+	void SetPosition(const int32_t& x, const int32_t& y);
+
+
+	/**
+	 * @brief 게임 윈도우 위치를 얻습니다.
+	 * 
+	 * @note 이때, 단위는 픽셀단위입니다.
+	 *
+	 * @param outX[out] 스크린 상의 게임 윈도우 왼쪽 상단의 X좌표입니다.
+	 * @param outY[out] 스크린 상의 게임 윈도우 왼쪽 상단의 Y좌표입니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_GetWindowPosition
+	 */
+	void GetPosition(int32_t& outX, int32_t& outY);
+
+
+	/**
+	 * @brief 게임 윈도우의 크기를 변경합니다.
+	 * 
+	 * @note 이때, 단위는 픽셀단위입니다.
+	 *
+	 * @param width 스크린 상의 게임 윈도우의 가로 크기입니다.
+	 * @param height 스크린 상의 게임 윈도우의 세로 크기입니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_SetWindowSize
+	 */
+	void SetSize(const int32_t& width, const int32_t& height);
+
+
+	/**
+	 * @brief 게임 윈도우의 크기를 얻습니다.
+	 * 
+	 * @note 이때, 단위는 픽셀단위입니다.
+	 *
+	 * @param outWidth 스크린 상의 게임 윈도우의 가로 크기입니다.
+	 * @param outHeight 스크린 상의 게임 윈도우의 세로 크기입니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_GetWindowSize
+	 */
+	void GetSize(int32_t& outWidth, int32_t& outHeight);
+
+
+	/**
+	 * @brief 게임 윈도우의 타이틀을 설정합니다.
+	 *
+	 * @param title 설정할 게임 윈도우 타이틀입니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_SetWindowTitle
+	 */
+	void SetTitle(const std::string& title);
+
+
+	/**
+	 * @brief 게임 윈도우의 타이틀을 얻습니다.
+	 *
+	 * @return 게임 윈도우 타이틀을 반환합니다.
+	 *
+	 * @see https://wiki.libsdl.org/SDL_GetWindowTitle
+	 */
+	std::string GetTitle();
+
+
+	/**
+	 * @brief 게임 윈도우를 화면에 표시합니다.
+	 */
+	void Show();
+
+
+	/**
+	 * @brief 게임 윈도우를 화면에서 숨깁니다.
+	 */
+	void Hide();
+
+
+	/**
+	 * @brief 윈도우의 크기 비율을 얻습니다.
+	 * 
+	 * @note 이때 비율 값은 가로 / 세로 입니다.
+	 *
+	 * @return 게임 윈도우의 크기 비율을 반환합니다.
+	 */
+	float GetAspectRatio();
+
+
+	/**
+	 * SDL윈도우의 포인터를 반환합니다.
+	 *
+	 * @return SDL 윈도우의 포인터를 반환합니다.
+	 */
+	SDL_Window* GetWindow() { return window_; }
+
+
+private:
+	/**
+	 * SDL 윈도우 포인터입니다.
+	 */
+	SDL_Window* window_ = nullptr;
+};
