2e5bdbd5e1ba1be9576d78a5fc3b3eef58183913
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-25 10:38:16 +0900

    [DEV] 위치와 색상 정보를 포함한 정점에 생성자 및 대입 연산자 추가

--- Game/Public/Vertex.h
+++ Game/Public/Vertex.h
@@ -13,7 +13,88 @@ namespace Vertex
 	 */
 	struct PositionColor
 	{
-		DirectX::XMFLOAT3 position; /** 정점의 위치입니다. */
-		DirectX::XMFLOAT4 color;    /** 정점의 색상입니다. */
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 기본 생성자입니다.
+		 */
+		PositionColor() : position(0.0f, 0.0f, 0.0f), color(0.0f, 0.0f, 0.0f, 0.0f) {}
+
+
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 생성자입니다.
+		 * 
+		 * @param p 정점의 위치입니다.
+		 * @param c 정점의 색상입니다.
+		 */
+		PositionColor(const DirectX::XMFLOAT3& p, const DirectX::XMFLOAT4& c)
+			: position(p)
+			, color(c) {}
+
+
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 복사 생성자입니다.
+		 * 
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		PositionColor(PositionColor&& instance) noexcept
+			: position(instance.position)
+			, color(instance.color) {}
+
+
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 복사 생성자입니다.
+		 *
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		PositionColor(const PositionColor& instance)
+			: position(instance.position)
+			, color(instance.color) {}
+
+
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 대입 연산자입니다.
+		 * 
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 * 
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		PositionColor& operator=(PositionColor&& instance) noexcept
+		{
+			if (this == &instance) return *this;
+
+			position = instance.position;
+			color = instance.color;
+
+			return *this;
+		}
+
+
+		/**
+		 * @brief 위치와 색상 정보를 포함한 정점의 대입 연산자입니다.
+		 *
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 *
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		PositionColor& operator=(const PositionColor& instance)
+		{
+			if (this == &instance) return *this;
+
+			position = instance.position;
+			color = instance.color;
+
+			return *this;
+		}
+
+
+		/** 
+		 * 정점의 위치입니다. 
+		 */
+		DirectX::XMFLOAT3 position;
+
+
+		/**
+		 * 정점의 색상입니다.
+		 */
+		DirectX::XMFLOAT4 color;
 	};
 }
