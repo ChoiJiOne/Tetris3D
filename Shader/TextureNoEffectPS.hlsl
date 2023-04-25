d853a5fae18a7dd8f0f5ab188239fa2845abf2c1
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-25 13:55:26 +0900

    [DEV] 효과 없이 정점 텍스처만 렌더링하는 셰이더 추가

---
+++ Shader/TextureNoEffectPS.hlsl
@@ -0,0 +1,22 @@
+struct PS_INPUT
+{
+    float4 position : SV_POSITION;
+    float2 uv : TEXCOORD0;
+};
+
+struct PS_OUTPUT
+{
+    float4 color : SV_TARGET0;
+};
+
+Texture2D<float4> textureResource : register(t0);
+SamplerState linearSamplerState : register(s0);
+
+PS_OUTPUT main(PS_INPUT input)
+{
+    PS_OUTPUT output;
+    
+    output.color = textureResource.Sample(linearSamplerState, input.uv);
+    
+    return output;
+}
