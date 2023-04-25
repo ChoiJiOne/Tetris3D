e4a87825e09b92370722dec7fdd15b0c836bd10d
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-25 11:15:40 +0900

    [DEV] 생성자의 정점 버퍼와 인덱스 버퍼 생성 부분을 메서드로 분리

--- Game/Private/StaticMesh.cpp
+++ Game/Private/StaticMesh.cpp
@@ -5,35 +5,19 @@ StaticMesh::StaticMesh(ID3D11Device* device, const std::vector<Vertex::PositionC
 	vertexByteSize_ = sizeof(Vertex::PositionColor);
 	countOfIndex_ = static_cast<uint32_t>(indices.size());
 
-	D3D11_BUFFER_DESC vertexBufferDesc;
-	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	vertexBufferDesc.ByteWidth = vertexByteSize_ * static_cast<uint32_t>(vertices.size());
-	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	vertexBufferDesc.CPUAccessFlags = 0;
-	vertexBufferDesc.MiscFlags = 0;
-	vertexBufferDesc.StructureByteStride = 0;
+	CHECK_HR(CreateVertexBuffer(
+		device, 
+		vertexByteSize_ * static_cast<uint32_t>(vertices.size()), 
+		reinterpret_cast<const void*>(&vertices[0]), 
+		&vertexBuffer_
+	), "failed to create vertex buffer...");
 
-	D3D11_SUBRESOURCE_DATA vertexData;
-	vertexData.pSysMem = reinterpret_cast<const void*>(&vertices[0]);
-	vertexData.SysMemPitch = 0;
-	vertexData.SysMemSlicePitch = 0;
-
-	CHECK_HR(device->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer_), "failed to create vertex buffer...");
-
-	D3D11_BUFFER_DESC indexBufferDesc;
-	indexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
-	indexBufferDesc.ByteWidth = sizeof(uint32_t) * static_cast<uint32_t>(indices.size());
-	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
-	indexBufferDesc.CPUAccessFlags = 0;
-	indexBufferDesc.MiscFlags = 0;
-	indexBufferDesc.StructureByteStride = 0;
-
-	D3D11_SUBRESOURCE_DATA indexData;
-	indexData.pSysMem = reinterpret_cast<const void*>(&indices[0]);
-	indexData.SysMemPitch = 0;
-	indexData.SysMemSlicePitch = 0;
-
-	CHECK_HR(device->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer_), "failed to create index buffer...");
+	CHECK_HR(CreateIndexBuffer(
+		device, 
+		sizeof(uint32_t) * static_cast<uint32_t>(indices.size()), 
+		reinterpret_cast<const void*>(&indices[0]), 
+		&indexBuffer_
+	), "failed to create index buffer...");
 }
 
 StaticMesh::~StaticMesh()
@@ -51,4 +35,40 @@ void StaticMesh::Draw(ID3D11DeviceContext* context)
 	context->IASetIndexBuffer(indexBuffer_, DXGI_FORMAT_R32_UINT, 0);
 	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	context->DrawIndexed(countOfIndex_, 0, 0);
+}
+
+HRESULT StaticMesh::CreateVertexBuffer(ID3D11Device* device, uint32_t byteWidth, const void* bufferPtr, ID3D11Buffer** outVertexBuffer)
+{
+	D3D11_BUFFER_DESC vertexBufferDesc;
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = byteWidth;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA vertexData;
+	vertexData.pSysMem = bufferPtr;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	return device->CreateBuffer(&vertexBufferDesc, &vertexData, outVertexBuffer);
+}
+
+HRESULT StaticMesh::CreateIndexBuffer(ID3D11Device* device, uint32_t byteWidth, const void* bufferPtr, ID3D11Buffer** outIndexBuffer)
+{
+	D3D11_BUFFER_DESC indexBufferDesc;
+	indexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	indexBufferDesc.ByteWidth = byteWidth;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA indexData;
+	indexData.pSysMem = bufferPtr;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	return device->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer_);
 }
--- Game/Public/StaticMesh.h
+++ Game/Public/StaticMesh.h
@@ -19,6 +19,8 @@ public:
 	 * @param device 버퍼를 생성할 때 사용할 디바이스입니다.
 	 * @param vertices 정적 메시의 정점 목록입니다.
 	 * @param indices 정적 메시의 인덱스 목록입니다.
+	 * 
+	 * @throws 정점 생성에 실패하면 C++ 표준 예외를 던집니다.
 	 */
 	StaticMesh(ID3D11Device* device, const std::vector<Vertex::PositionColor>& vertices, const std::vector<uint32_t>& indices);
 
@@ -41,6 +43,33 @@ public:
 	 * @param context 렌더링을 수행할 컨텍스트입니다.
 	 */
 	void Draw(ID3D11DeviceContext* context);
+
+
+private:
+	/**
+	 * @brief 정점 버퍼를 생성합니다.
+	 * 
+	 * @param device 버퍼 생성에 사용할 디바이스입니다.
+	 * @param byteWidth 정점 버퍼의 바이트 크기입니다.
+	 * @param bufferPtr 정점 버퍼의 포인터입니다.
+	 * @param outVertexBuffer[out] 생성된 정점 버퍼입니다.
+	 * 
+	 * @return 정점 버퍼 생성 결과를 반환합니다. 생성에 성공하면 S_OK, 그렇지 않다면 그 이외의 값을 반환합니다.
+	 */
+	HRESULT CreateVertexBuffer(ID3D11Device* device, uint32_t byteWidth, const void* bufferPtr, ID3D11Buffer** outVertexBuffer);
+
+
+	/**
+	 * @brief 인덱스 버퍼를 생성합니다.
+	 * 
+	 * @param device 버퍼 생성에 사용할 디바이스입니다.
+	 * @param byteWidth 인덱스 버퍼의 바이트 크기입니다.
+	 * @param bufferPtr 인덱스 버퍼의 포인터입니다.
+	 * @param outIndexBuffer[out] 생성된 인덱스 버퍼입니다.
+	 * 
+	 * @return 인덱스 버퍼 생성 결과를 반환합니다. 생성에 성공하면 S_OK, 그렇지 않다면 그 이외의 값을 반환합니다.
+	 */
+	HRESULT CreateIndexBuffer(ID3D11Device* device, uint32_t byteWidth, const void* bufferPtr, ID3D11Buffer** outIndexBuffer);
 
 
 private:
