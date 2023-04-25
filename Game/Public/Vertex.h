1c58c83196c4e9b9521b1325e52e414c0833ab73
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-25 12:24:10 +0900

    [DEV] 위치와 텍스처 위치 정보를 나타내는 정점 정의 추가

--- Game/Public/Vertex.h
+++ Game/Public/Vertex.h
@@ -99,4 +99,97 @@ namespace Vertex
 		 */
 		DirectX::XMFLOAT4 color_;
 	};
+
+
+	/**
+	 * @brief 위치와 텍스처 위치 정보를 포함한 정점입니다.
+	 */
+	class PositionUV
+	{
+	public:
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 기본 생성자입니다.
+		 */
+		PositionUV() : position_(0.0f, 0.0f, 0.0f), uv_(0.0f, 0.0f) {}
+
+
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 생성자입니다.
+		 *
+		 * @param position 정점의 위치입니다.
+		 * @param uv 정점의 텍스처 위치입니다.
+		 */
+		PositionUV(const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT2& uv)
+			: position_(position)
+			, uv_(uv) {}
+
+
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 복사 생성자입니다.
+		 *
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		PositionUV(PositionUV&& instance) noexcept
+			: position_(instance.position_)
+			, uv_(instance.uv_) {}
+
+
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 복사 생성자입니다.
+		 *
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		PositionUV(const PositionUV& instance)
+			: position_(instance.position_)
+			, uv_(instance.uv_) {}
+
+
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 대입 연산자입니다.
+		 *
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 *
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		PositionUV& operator=(PositionUV&& instance) noexcept
+		{
+			if (this == &instance) return *this;
+
+			position_ = instance.position_;
+			uv_ = instance.uv_;
+
+			return *this;
+		}
+
+
+		/**
+		 * @brief 위치와 텍스처 위치 정보를 포함한 정점의 대입 연산자입니다.
+		 *
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 *
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		PositionUV& operator=(const PositionUV& instance)
+		{
+			if (this == &instance) return *this;
+
+			position_ = instance.position_;
+			uv_ = instance.uv_;
+
+			return *this;
+		}
+
+
+	private:
+		/**
+		 * @brief 정점의 위치입니다.
+		 */
+		DirectX::XMFLOAT3 position_;
+
+
+		/**
+		 * @brief 정점의 텍스처 위치입니다.
+		 */
+		DirectX::XMFLOAT2 uv_;
+	};
 }
