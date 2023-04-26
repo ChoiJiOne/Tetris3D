b493ef675853613f799163598eb9c4e6e73bd6ed
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-26 14:00:53 +0900

    [DEV] TextureNoEffectShader가 EffectShader를 상속받도록 구조 변경
    - 필요 없는 멤버 변수, 멤버 함수, 해더 선언 삭제

--- Game/Private/TextureNoEffectShader.cpp
+++ Game/Private/TextureNoEffectShader.cpp
@@ -1,6 +1,3 @@
-#include <d3dcompiler.h>
-
-#include "StringHelper.hpp"
 #include "Texture2D.h"
 #include "TextureNoEffectShader.h"
 
@@ -52,11 +49,6 @@ TextureNoEffectShader::~TextureNoEffectShader()
 {
 	SAFE_RELEASE(linearSamplerState_);
 	SAFE_RELEASE(everyFrameBuffer_);
-	SAFE_RELEASE(inputLayout_);
-	SAFE_RELEASE(pixelShader_);
-	SAFE_RELEASE(pixelShaderSource_);
-	SAFE_RELEASE(vertexShader_);
-	SAFE_RELEASE(vertexShaderSource_);
 }
 
 void TextureNoEffectShader::SetTexture(Texture2D* texture)
@@ -67,9 +59,7 @@ void TextureNoEffectShader::SetTexture(Texture2D* texture)
 
 void TextureNoEffectShader::Bind(ID3D11DeviceContext* context)
 {
-	context->IASetInputLayout(inputLayout_);
-	context->VSSetShader(vertexShader_, nullptr, 0);
-	context->PSSetShader(pixelShader_, nullptr, 0);
+	EffectShader::Bind(context);
 
 	D3D11_MAPPED_SUBRESOURCE everyFrameBufferMappedResource;
 	if (SUCCEEDED(context->Map(everyFrameBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &everyFrameBufferMappedResource)))
@@ -86,75 +76,4 @@ void TextureNoEffectShader::Bind(ID3D11DeviceContext* context)
 
 	context->PSSetShaderResources(textureResourcebindSlot_, 1, &textureResource_);
 	context->PSSetSamplers(samplerStateBindSlot_, 1, &linearSamplerState_);
-}
-
-HRESULT TextureNoEffectShader::CompileShaderFromFile(const std::wstring& sourcePath, const std::string& entryPoint, const std::string& shaderModel, ID3DBlob** blob)
-{
-	HRESULT hr = S_OK;
-
-	DWORD shaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
-
-#if defined(DEBUG) || defined(_DEBUG)
-	shaderFlags |= D3DCOMPILE_DEBUG;
-	shaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
-#endif
-
-	hr = D3DCompileFromFile(
-		sourcePath.c_str(),
-		nullptr,
-		nullptr,
-		entryPoint.c_str(),
-		shaderModel.c_str(),
-		shaderFlags,
-		0,
-		blob,
-		nullptr
-	);
-
-	return hr;
-}
-
-HRESULT TextureNoEffectShader::CreateVertexShaderFromFile(ID3D11Device* device, const std::wstring& sourcePath, ID3DBlob** vertexShaderSource, ID3D11VertexShader** vertexShader)
-{
-	HRESULT hr = CompileShaderFromFile(sourcePath, "main", "vs_5_0", vertexShaderSource);
-
-	if (SUCCEEDED(hr))
-	{
-		hr = device->CreateVertexShader(
-			(*vertexShaderSource)->GetBufferPointer(),
-			(*vertexShaderSource)->GetBufferSize(),
-			nullptr,
-			vertexShader
-		);
-	}
-
-	return hr;
-}
-
-HRESULT TextureNoEffectShader::CreatePixelShaderFromFile(ID3D11Device* device, const std::wstring& sourcePath, ID3DBlob** pixelShaderSource, ID3D11PixelShader** pixelShader)
-{
-	HRESULT hr = CompileShaderFromFile(sourcePath, "main", "ps_5_0", pixelShaderSource);
-
-	if (SUCCEEDED(hr))
-	{
-		hr = device->CreatePixelShader(
-			(*pixelShaderSource)->GetBufferPointer(),
-			(*pixelShaderSource)->GetBufferSize(),
-			nullptr,
-			pixelShader
-		);
-	}
-
-	return hr;
-}
-
-HRESULT TextureNoEffectShader::CreateInputLayout(ID3D11Device* device, ID3DBlob* vertexShaderSource, const std::vector<D3D11_INPUT_ELEMENT_DESC>& inputLayoutElements, ID3D11InputLayout** inputLayout)
-{
-	return device->CreateInputLayout(
-		&inputLayoutElements[0],
-		static_cast<uint32_t>(inputLayoutElements.size()),
-		vertexShaderSource->GetBufferPointer(),
-		vertexShaderSource->GetBufferSize(),
-		inputLayout
-	);
 }
