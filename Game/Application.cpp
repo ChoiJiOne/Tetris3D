de88b0a55c154fca2f2c169a78c23c0bb6920e09
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-04-27 18:58:27 +0900

    [DEV] RunApplication 부분 정리 및 로딩 부분 추가

--- Game/Application.cpp
+++ Game/Application.cpp
@@ -12,13 +12,124 @@
 #include "GameTimer.h"
 #include "GeometryGenerator.h"
 #include "TextureNoEffectShader.h"
+#include "Block.h"
+#include "FixCamera.h"
 #include "Window.h"
 #include "InputManager.h"
+#include "WorldManager.h"
 #include "StringHelper.hpp"
 #include "RenderManager.h"
 #include "StaticMesh.h"
 #include "Texture2D.h"
 #include "Vertex.h"
+
+
+/**
+ * @brief 셰이더 파일을 로딩합니다.
+ * 
+ * @throws 셰이더 파일 로딩에 실패하면 C++ 표준 예외를 던집니다.
+ */
+void LoadShader()
+{
+	std::wstring shaderSourcePath = StringHelper::Convert(CommandLine::GetValue("Shader"));
+
+	ContentManager::Get().AddEffectShader(
+		"ColorNoEffectShader",
+		std::make_unique<ColorNoEffectShader>(
+			RenderManager::Get().GetDevice(),
+			shaderSourcePath + L"ColorNoEffectVS.hlsl",
+			shaderSourcePath + L"ColorNoEffectPS.hlsl"
+		)
+	);
+
+	ContentManager::Get().AddEffectShader(
+		"TextureNoEffectShader",
+		std::make_unique<TextureNoEffectShader>(
+			RenderManager::Get().GetDevice(),
+			shaderSourcePath + L"TextureNoEffectVS.hlsl",
+			shaderSourcePath + L"TextureNoEffectPS.hlsl"
+		)
+	);
+}
+
+
+/**
+ * @brief 텍스처 파일을 로딩합니다.
+ * 
+ * @throws 텍스처 파일 로딩에 실패하면 C++ 표준 예외를 던집니다.
+ */
+void LoadTexture()
+{
+	std::array<std::string, 9> textures = {
+		"BlueBlock",
+		"CyanBlock",
+		"GrayBlock",
+		"GreenBlock",
+		"MagentaBlock",
+		"OrangeBlock",
+		"PurpleBlock",
+		"RedBlock",
+		"YellowBlock",
+	};
+
+	std::string texturePath = CommandLine::GetValue("Content");
+	for (const auto& texture : textures)
+	{
+		ContentManager::Get().AddTexture2D(
+			texture,
+			std::make_unique<Texture2D>(
+				RenderManager::Get().GetDevice(),
+				StringHelper::Format("%s%s%s", texturePath.c_str(), texture.c_str(), ".png")
+			)
+		);
+	}
+}
+
+
+/**
+ * @brief 정적 메시를 로딩합니다.
+ *
+ * @throws 정적 메시 로딩에 실패하면 C++ 표준 예외를 던집니다.
+ */
+void LoadStaticMesh()
+{
+	std::vector<Vertex::PositionUV> vertices;
+	std::vector<uint32_t> indices;
+	GeometryGenerator::CreateBox(2.0f, 2.0f, 2.0f, vertices, indices);
+
+	ContentManager::Get().AddStaticMesh(
+		"Block",
+		std::make_unique<StaticMesh>(RenderManager::Get().GetDevice(), vertices, indices)
+	);
+}
+
+
+/**
+ * @brief 게임 오브젝트를 로딩합니다.
+ * 
+ * @throws 게임 오브젝트 로딩에 실패하면 C++ 표준 예외를 던집니다.
+ */
+void LoadGameObject()
+{
+	float aspectRatio = 1000.0f / 800.0f;
+
+	WorldManager::Get().AddGameObject(
+		"FixCamera", 
+		std::make_unique<FixCamera>(
+		DirectX::XMFLOAT3(+6.0f, +6.0f, -6.0f),
+		DirectX::XMFLOAT3(+0.0f, +0.0f, +0.0f),
+		DirectX::XMFLOAT3(+0.0f, +1.0f, +0.0f),
+		DirectX::XM_PIDIV4,
+		aspectRatio
+	));
+
+	WorldManager::Get().AddGameObject(
+		"Block", 
+		std::make_unique<Block>(
+		DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f),
+		Block::EColor::PURPLE
+	));
+}
 
 
 /**
@@ -38,51 +149,22 @@ void RunApplication(int32_t argc, char** argv)
 
 	CHECK(SDL_Init(SDL_INIT_EVERYTHING) == 0, "failed to initialize SDL2...");
 	
-	std::unique_ptr<Window> window = std::make_unique<Window>("Tetris3D", 200, 200, 1000, 800, EWindowFlags::SHOWN | EWindowFlags::RESIZABLE);
-
-	bool bIsDone = false;
+	std::unique_ptr<Window> window = std::make_unique<Window>(
+		"Tetris3D", 
+		200, 200, 
+		1000, 800, 
+		EWindowFlags::SHOWN | EWindowFlags::RESIZABLE
+	);
 
 	InputManager::Get().Setup();
 	RenderManager::Get().Setup(window.get());
+	ContentManager::Get().Setup();
+	WorldManager::Get().Setup();
 
-	InputManager::Get().BindWindowEventAction(EWindowEvent::CLOSE, [&]() { bIsDone = true; });
-	InputManager::Get().BindWindowEventAction(EWindowEvent::RESIZED, [&]() { RenderManager::Get().Resize(); });
-
-	std::wstring shaderSourcePath = StringHelper::Convert(CommandLine::GetValue("Shader"));
-	TextureNoEffectShader* effectShader = reinterpret_cast<TextureNoEffectShader*>(ContentManager::Get().AddEffectShader(
-		"TextureNoEffectShader", 
-		std::make_unique<TextureNoEffectShader>(
-			RenderManager::Get().GetDevice(),
-			shaderSourcePath + L"TextureNoEffectVS.hlsl",
-			shaderSourcePath + L"TextureNoEffectPS.hlsl"
-		)
-	));
-
-	DirectX::XMVECTOR eye = DirectX::XMVectorSet(6.0f, 6.0f, -6.0f, 0.0f);
-	DirectX::XMVECTOR at = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
-	DirectX::XMVECTOR up = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
-
-	effectShader->SetWorldMatrix(DirectX::XMMatrixIdentity());
-	effectShader->SetViewMatrix(DirectX::XMMatrixLookAtLH(eye, at, up));
-	effectShader->SetProjectionMatrix(DirectX::XMMatrixPerspectiveFovLH(DirectX::XM_PIDIV4, window->GetAspectRatio(), 0.01f, 1000.0f));
-
-	std::string texturePath = CommandLine::GetValue("Content");
-	Texture2D* texture = ContentManager::Get().AddTexture2D(
-		"RedBlock", 
-		std::make_unique<Texture2D>(
-			RenderManager::Get().GetDevice(),
-			texturePath + "RedBlock.png"
-		)
-	);
-
-	std::vector<Vertex::PositionUV> vertices;
-	std::vector<uint32_t> indices;
-	GeometryGenerator::CreateBox(2.0f, 2.0f, 2.0f, vertices, indices);
-
-	StaticMesh* mesh = ContentManager::Get().AddStaticMesh(
-		"Block",
-		std::make_unique<StaticMesh>(RenderManager::Get().GetDevice(), vertices, indices)
-	);
+	LoadShader();
+	LoadTexture();
+	LoadStaticMesh();
+	LoadGameObject();
 
 	GameTimer gameTimer;
 	gameTimer.Reset();
@@ -90,6 +172,16 @@ void RunApplication(int32_t argc, char** argv)
 	RenderManager::Get().SetAlphaBlend(true);
 	RenderManager::Get().SetDepthBuffer(true);
 	RenderManager::Get().SetFillMode(true);
+
+	bool bIsDone = false;
+
+	InputManager::Get().BindWindowEventAction(EWindowEvent::CLOSE, [&]() { bIsDone = true; });
+	InputManager::Get().BindWindowEventAction(EWindowEvent::RESIZED, [&]() {
+		RenderManager::Get().Resize();
+	
+		FixCamera* fixCamera = reinterpret_cast<FixCamera*>(WorldManager::Get().GetGameObject("FixCamera"));
+		fixCamera->SetAspectRatio(window->GetAspectRatio());
+	});
 
 	while (!bIsDone)
 	{
@@ -99,13 +191,13 @@ void RunApplication(int32_t argc, char** argv)
 		RenderManager::Get().BeginFrame(1.0f, 1.0f, 1.0f, 1.0f);
 		RenderManager::Get().SetWindowViewport();
 
-		effectShader->SetTexture(texture);
-		effectShader->Bind(RenderManager::Get().GetContext());
-		mesh->Draw(RenderManager::Get().GetContext());
+		WorldManager::Get().Tick(gameTimer.GetDeltaSeconds());
 
 		RenderManager::Get().EndFrame();
 	}
 
+	WorldManager::Get().Cleanup();
+	ContentManager::Get().Cleanup();
 	RenderManager::Get().Cleanup();
 	InputManager::Get().Cleanup();
 
