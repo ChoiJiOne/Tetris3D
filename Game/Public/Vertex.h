293b9f5e31ce0a1efa1fd70cd88acd8c2831ac87
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-04-29 20:09:49 +0900

    [DEV] 위치 정보만 가진 정점 정의 추가

--- Game/Public/Vertex.h
+++ Game/Public/Vertex.h
@@ -9,6 +9,87 @@
 namespace Vertex
 {
 	/**
+	 * @brief 위치 정보만 포함한 정점입니다.
+	 */
+	class Position
+	{
+	public:
+		/**
+		 * @brief 위치 정보만 포함한 정점의 기본 생성자입니다.
+		 */
+		Position() : position_(0.0f, 0.0f, 0.0f) {}
+		
+
+		/**
+		 * @brief 위치 정보만 포함한 정점의 생성자입니다.
+		 *
+		 * @param position 정점의 위치입니다.
+		 */
+		Position(const DirectX::XMFLOAT3& position)
+			: position_(position) {}
+
+
+		/**
+		 * @brief 위치 정보만 포함한 정점의 복사 생성자입니다.
+		 *
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		Position(Position&& instance) noexcept
+			: position_(instance.position_) {}
+
+
+		/**
+		 * @brief 위치 정보만 포함한 정점의 복사 생성자입니다.
+		 *
+		 * @param instance 복사할 정점의 인스턴스입니다.
+		 */
+		Position(const Position& instance)
+			: position_(instance.position_) {}
+
+
+		/**
+		 * @brief 위치 정보만 포함한 정점의 대입 연산자입니다.
+		 *
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 *
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		Position& operator=(Position&& instance) noexcept
+		{
+			if (this == &instance) return *this;
+
+			position_ = instance.position_;
+
+			return *this;
+		}
+
+
+		/**
+		 * @brief 위치 정보만 포함한 정점의 대입 연산자입니다.
+		 *
+		 * @param instacne 대입할 정점의 인스턴스입니다.
+		 *
+		 * @return 대입한 객체를 반환합니다.
+		 */
+		Position& operator=(const Position& instance)
+		{
+			if (this == &instance) return *this;
+
+			position_ = instance.position_;
+
+			return *this;
+		}
+
+
+	private:
+		/**
+		 * @brief 정점의 위치입니다.
+		 */
+		DirectX::XMFLOAT3 position_;
+	};
+
+
+	/**
 	 * @brief 위치와 색상 정보를 포함한 정점입니다.
 	 */
 	class PositionColor
