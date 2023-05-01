52b89ef0c8b4c28fa640778192ba5d744f0b96f6
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-01 16:22:50 +0900

    [DEV] 테트리스 게임 내의 보드 블럭을 구성하고 렌더링하는 기능 추가

---
+++ Game/Private/Board.cpp
@@ -0,0 +1,130 @@
+#include "ContentManager.h"
+#include "FixCamera.h"
+#include "RenderManager.h"
+#include "InputManager.h"
+#include "StaticMesh.h"
+#include "Texture2D.h"
+#include "TextureNoEffectShader.h"
+#include "WorldManager.h"
+#include "Board.h"
+
+Board::Board(
+	int32_t updateOrder, 
+	bool bIsActive, 
+	const DirectX::XMFLOAT3& basePosition, 
+	float blockSize, 
+	const Block::EColor& blockColor,
+	int32_t countRowBlock,
+	int32_t countColBlock
+) : GameObject(updateOrder, bIsActive)
+, basePosition_(basePosition)
+, countRowBlock_(countRowBlock)
+, countColBlock_(countColBlock)
+{
+	blockStaticMesh_ = ContentManager::Get().GetStaticMesh("Block");
+	mappingBlockColorToTextures_ = {
+		{ Block::EColor::BLUE,    nullptr },
+		{ Block::EColor::CYAN,    nullptr },
+		{ Block::EColor::GRAY,    nullptr },
+		{ Block::EColor::GREEN,   nullptr },
+		{ Block::EColor::MAGENTA, nullptr },
+		{ Block::EColor::ORANGE,  nullptr },
+		{ Block::EColor::PINK,    nullptr },
+		{ Block::EColor::PURPLE,  nullptr },
+		{ Block::EColor::RED,     nullptr },
+		{ Block::EColor::YELLOW,  nullptr },
+	};
+
+	for (auto& mappingBlockColorToTexture : mappingBlockColorToTextures_)
+	{
+		Block::EColor color = mappingBlockColorToTexture.first;
+		mappingBlockColorToTexture.second = ContentManager::Get().GetTexture2D(Block::GetColorTextureSignature(color));
+	}
+
+	outlineBlocks_ = GenerateOutlineBlocks(basePosition_, blockSize, blockColor, countRowBlock_, countColBlock_);
+	backgroundBlocks_ = GenerateBackgroundBlocks(basePosition_, blockSize, blockColor, countRowBlock_, countColBlock_);
+}
+
+Board::~Board()
+{
+}
+
+void Board::Tick(float deltaSeconds)
+{
+	DrawBlocks(backgroundBlocks_);
+	DrawBlocks(outlineBlocks_);
+}
+
+std::vector<Block> Board::GenerateOutlineBlocks(
+	const DirectX::XMFLOAT3& basePosition, 
+	float blockSize, 
+	const Block::EColor& blockColor,
+	int32_t countRowBlock,
+	int32_t countColBlock
+)
+{
+	std::vector<Block> blocks;
+
+	for (int32_t row = 0; row < countRowBlock; ++row)
+	{
+		for (int32_t col = 0; col < countColBlock; ++col)
+		{
+			if ((row == 0 || row == countRowBlock - 1) || (col == 0 || col == countColBlock - 1))
+			{
+				float x = basePosition.x + static_cast<float>(col) * blockSize;
+				float y = basePosition.y - static_cast<float>(row) * blockSize;
+				float z = basePosition.z;
+
+				blocks.push_back(Block(DirectX::XMFLOAT3(x, y, z), blockSize, blockColor));
+			}
+		}
+	}
+
+	return blocks;
+}
+
+std::vector<Block> Board::GenerateBackgroundBlocks(
+	const DirectX::XMFLOAT3& basePosition, 
+	float blockSize, 
+	const Block::EColor& blockColor,
+	int32_t countRowBlock,
+	int32_t countColBlock
+)
+{
+	std::vector<Block> blocks(countColBlock * countRowBlock);
+
+	for (int32_t row = 0; row < countRowBlock; ++row)
+	{
+		for (int32_t col = 0; col < countColBlock; ++col)
+		{
+			float x = basePosition.x + static_cast<float>(col) * blockSize;
+			float y = basePosition.y - static_cast<float>(row) * blockSize;
+			float z = basePosition.z + blockSize;
+
+			int32_t offset = col + row * (countColBlock);
+			blocks[offset] = Block(DirectX::XMFLOAT3(x, y, z), blockSize, blockColor);
+		}
+	}
+
+	return blocks;
+}
+
+void Board::DrawBlocks(const std::vector<Block>& blocks)
+{
+	FixCamera* fixCamera = reinterpret_cast<FixCamera*>(WorldManager::Get().GetGameObject("FixCamera"));
+	TextureNoEffectShader* effectShader = reinterpret_cast<TextureNoEffectShader*>(ContentManager::Get().GetEffectShader("TextureNoEffectShader"));
+
+	for (const Block& block : blocks)
+	{
+		DirectX::XMFLOAT3 position = block.GetPosition();
+
+		effectShader->SetWorldMatrix(DirectX::XMMatrixTranslation(position.x, position.y, position.z));
+		effectShader->SetViewMatrix(fixCamera->GetViewMatrix());
+		effectShader->SetProjectionMatrix(fixCamera->GetProjectionMatrix());
+
+		effectShader->SetTexture(mappingBlockColorToTextures_[block.GetColor()]);
+		effectShader->Bind(RenderManager::Get().GetContext());
+
+		blockStaticMesh_->Draw(RenderManager::Get().GetContext());
+	}
+}
---
+++ Game/Public/Board.h
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#include "Block.h"
+#include "GameObject.h"
+
+class StaticMesh;
+class Texture2D;
+
+
+/**
+ * @brief 테트리스 게임의 보드입니다.
+ */
+class Board : public GameObject
+{
+public:
+	/**
+	 * @brief 보드의 생성자입니다.
+	 * 
+	 * @param updateOrder 보드의 업데이트 순위입니다.
+	 * @param bIsActive 보드의 활성화 여부입니다.
+	 * @param basePosition 보드의 기준 좌표입니다.
+	 * @param blockSize 보드 내 블럭의 크기입니다.
+	 * @param blockColor 보드 내 블럭의 색상입니다.
+	 * @param countRowBlock 보드 내 세로 블럭 수입니다.
+	 * @param countColBlock 보드 내 가로 블럭 수입니다.
+	 */
+	Board(
+		int32_t updateOrder,
+		bool bIsActive,
+		const DirectX::XMFLOAT3& basePosition,
+		float blockSize,
+		const Block::EColor& blockColor,
+		int32_t countRowBlock,
+		int32_t countColBlock
+	);
+
+
+	/**
+	 * @brief 보드의 가상 소멸자입니다.
+	 */
+	virtual ~Board();
+
+
+	/**
+	 * @brief 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Board);
+
+
+	/**
+	 * @brief 보드를 업데이트합니다.
+	 * 
+	 * @param deltaSeconds 초단위 델타 시간값입니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+private:
+	/**
+	 * @brief 보드의 외곽선 블럭들을 생성합니다.
+	 * 
+	 * @param basePosition 보드의 기준 좌표입니다.
+	 * @param blockSize 보드 내 블럭의 크기입니다.
+	 * @param blockColor 보드 내 블럭의 색상입니다.
+	 * @param countRowBlock 보드 내 세로 블럭 수입니다.
+	 * @param countColBlock 보드 내 가로 블럭 수입니다.
+	 * 
+	 * @return 생성된 외곽선 블럭들을 반환합니다.
+	 */
+	std::vector<Block> GenerateOutlineBlocks(
+		const DirectX::XMFLOAT3& basePosition,
+		float blockSize,
+		const Block::EColor& blockColor,
+		int32_t countRowBlock,
+		int32_t countColBlock
+	);
+
+
+	/**
+	 * @brief 보드의 백그라운드 블럭들을 생성합니다.
+	 *
+	 * @param basePosition 보드의 기준 좌표입니다.
+	 * @param blockSize 보드 내 블럭의 크기입니다.
+	 * @param blockColor 보드 내 블럭의 색상입니다.
+	 * @param countRowBlock 보드 내 세로 블럭 수입니다.
+	 * @param countColBlock 보드 내 가로 블럭 수입니다.
+	 * 
+	 * @return 생성된 백그라운드 블럭들을 반환합니다.
+	 */
+	std::vector<Block> GenerateBackgroundBlocks(
+		const DirectX::XMFLOAT3& basePosition,
+		float blockSize,
+		const Block::EColor& blockColor,
+		int32_t countRowBlock,
+		int32_t countColBlock
+	);
+
+
+	/**
+	 * @brief 블럭들을 백버퍼에 그립니다.
+	 * 
+	 * @param blocks 백버퍼에 그릴 블럭들입니다.
+	 */
+	void DrawBlocks(const std::vector<Block>& blocks);
+
+
+private:
+	/**
+	 * @brief 보드의 기준 좌표입니다.
+	 */
+	DirectX::XMFLOAT3 basePosition_;
+
+
+	/**
+	 * @brief 보드에서 생성할 수 있는 세로 블럭 수입니다.
+	 */
+	int32_t countRowBlock_ = 0;
+
+
+	/**
+	 * @brief 보드에서 생성할 수 있는 가로 블럭 수입니다.
+	 */
+	int32_t countColBlock_ = 0;
+
+
+	/**
+	 * @brief 보드 내의 백그라운드 블럭입니다.
+	 */
+	std::vector<Block> backgroundBlocks_;
+
+
+	/**
+	 * @brief 보드 내의 외곽선 블럭입니다.
+	 */
+	std::vector<Block> outlineBlocks_;
+
+
+	/**
+	 * @brief 블럭의 정적 메시입니다.
+	 */
+	StaticMesh* blockStaticMesh_ = nullptr;
+
+
+	/**
+	 * @brief 블럭의 색상에 대응하는 텍스처입니다.
+	 */
+	std::unordered_map<Block::EColor, Texture2D*> mappingBlockColorToTextures_;
+};
