312f464edb763297101754edb395dac628c58a71
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-09 22:43:06 +0900

    [DEV] 평면 쿼드의 정점과 인덱스 버퍼를 생성하는 기능 추가

--- Game/Private/GeometryGenerator.cpp
+++ Game/Private/GeometryGenerator.cpp
@@ -1,5 +1,68 @@
 #include "GeometryGenerator.h"
 
+void GeometryGenerator::CreateQuadXY(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfWidth = width / 2.0f;
+	float halfHeight = height / 2.0f;
+
+	outVertices = {
+		Vertex::PositionUV(DirectX::XMFLOAT3(-halfWidth, -halfHeight, 0.0f), DirectX::XMFLOAT2(0.0f, 1.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(-halfWidth, +halfHeight, 0.0f), DirectX::XMFLOAT2(0.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(+halfWidth, +halfHeight, 0.0f), DirectX::XMFLOAT2(1.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(+halfWidth, -halfHeight, 0.0f), DirectX::XMFLOAT2(1.0f, 1.0f)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
+
+void GeometryGenerator::CreateQuadYZ(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfWidth = width / 2.0f;
+	float halfHeight = height / 2.0f;
+
+	outVertices = {
+		Vertex::PositionUV(DirectX::XMFLOAT3(0.0f, -halfWidth, -halfHeight), DirectX::XMFLOAT2(0.0f, 1.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(0.0f, +halfWidth, -halfHeight), DirectX::XMFLOAT2(0.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(0.0f, +halfWidth, +halfHeight), DirectX::XMFLOAT2(1.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(0.0f, -halfWidth, +halfHeight), DirectX::XMFLOAT2(1.0f, 1.0f)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
+
+void GeometryGenerator::CreateQuadXZ(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfWidth = width / 2.0f;
+	float halfHeight = height / 2.0f;
+
+	outVertices = {
+		Vertex::PositionUV(DirectX::XMFLOAT3(-halfWidth, 0.0f, -halfHeight), DirectX::XMFLOAT2(0.0f, 1.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(-halfWidth, 0.0f, +halfHeight), DirectX::XMFLOAT2(0.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(+halfWidth, 0.0f, +halfHeight), DirectX::XMFLOAT2(1.0f, 0.0f)),
+		Vertex::PositionUV(DirectX::XMFLOAT3(+halfWidth, 0.0f, -halfHeight), DirectX::XMFLOAT2(1.0f, 1.0f)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
+
 void GeometryGenerator::CreateBox(float width, float height, float depth, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices)
 {
 	outVertices.resize(0);
--- Game/Public/GeometryGenerator.h
+++ Game/Public/GeometryGenerator.h
@@ -14,6 +14,39 @@ class GeometryGenerator
 {
 public:
 	/**
+	 * @brief 위치와 텍스처 좌표가 포함된 XY평면 쿼드를 생성합니다.
+	 *
+	 * @param width 쿼드의 가로 크기입니다.
+	 * @param height 쿼드의 세로 크기입니다.
+	 * @param outVertices[out] 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices[out] 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadXY(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices);
+
+
+	/**
+	 * @brief 위치와 텍스처 좌표가 포함된 YZ평면 쿼드를 생성합니다.
+	 *
+	 * @param width 쿼드의 가로 크기입니다.
+	 * @param height 쿼드의 세로 크기입니다.
+	 * @param outVertices[out] 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices[out] 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadYZ(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices);
+
+
+	/**
+	 * @brief 위치와 텍스처 좌표가 포함된 XZ평면 쿼드를 생성합니다.
+	 *
+	 * @param width 쿼드의 가로 크기입니다.
+	 * @param height 쿼드의 세로 크기입니다.
+	 * @param outVertices[out] 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices[out] 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadXZ(float width, float height, std::vector<Vertex::PositionUV>& outVertices, std::vector<uint32_t>& outIndices);
+
+	
+	/**
 	 * @brief 정점과 텍스처 좌표가 포함된 정점의 박스를 생성합니다.
 	 * 
 	 * @param width 박스의 가로 크기입니다.
