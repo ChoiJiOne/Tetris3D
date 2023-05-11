cb3347f24bb0d65fc5569554230a2bf142921f07
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-11 09:31:24 +0900

    [DEV] Widget 삭제된 변경 사항 적용

--- Game/Application.cpp
+++ Game/Application.cpp
@@ -17,7 +17,6 @@
 #include "InputManager.h"
 #include "PlayScene.h"
 #include "RenderManager.h"
-#include "Widget.h"
 #include "SpriteNoEffectShader.h"
 #include "StaticMesh.h"
 #include "StringHelper.hpp"
