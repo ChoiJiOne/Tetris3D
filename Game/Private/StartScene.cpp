e51367ac1d90ad2663debcf4e74aa63eff8e815a
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-14 17:30:27 +0900

    [DEV] UI 버튼의 이벤트 추가
    - 시작 버튼 클릭 시 시작 씬 내의 오브젝트를 비활성화
    - 종료 버튼 클릭 시 게임 종료

--- Game/Private/StartScene.cpp
+++ Game/Private/StartScene.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include "Button.h"
 #include "Label.h"
 #include "WorldManager.h"
@@ -13,51 +15,94 @@ void StartScene::Entry()
 
 	uiUpdateOrder_ = 4;
 
-	Label::ConstructorParam titleParam {
-		uiUpdateOrder_,
-		true,
-		DirectX::XMFLOAT2(0.0f, 0.4f),
-		"SeoulNamsanEB128",
-		L"TETRIS 3D",
-		DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f)
-	};
+	Label* titleLabel = reinterpret_cast<Label*>(WorldManager::Get().GetGameObject("TitleLabel"));
+	if (!titleLabel)
+	{
+		Label::ConstructorParam titleParam{
+			uiUpdateOrder_,
+			true,
+			DirectX::XMFLOAT2(0.0f, 0.4f),
+			"SeoulNamsanEB128",
+			L"TETRIS 3D",
+			DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f)
+		};
 
-	WorldManager::Get().AddGameObject("TitleLabel", std::make_unique<Label>(titleParam));
+		WorldManager::Get().AddGameObject("TitleLabel", std::make_unique<Label>(titleParam));
+	}
+	else
+	{
+		titleLabel->SetActive(true);
+	}
 
-	Button::ConstructorParam startButtonParam {
-		uiUpdateOrder_,
-		true,
-		DirectX::XMFLOAT2(0.0f, 0.0f),
-		200.0f, 
-		200.0f,
-		"Start",
-		1.0f,
-		0.5f,
-		0.95f,
-		[&]() {
-		}
-	};
+	Button* startButton = reinterpret_cast<Button*>(WorldManager::Get().GetGameObject("StartButton"));
+	if (!startButton)
+	{
+		Button::ConstructorParam startButtonParam{
+			uiUpdateOrder_,
+			true,
+			DirectX::XMFLOAT2(0.0f, 0.0f),
+			200.0f,
+			200.0f,
+			"Start",
+			1.0f,
+			0.5f,
+			0.95f,
+			[&]() {
+				Leave();
+			}
+		};
 
-	WorldManager::Get().AddGameObject("StartButton", std::make_unique<Button>(startButtonParam));
+		WorldManager::Get().AddGameObject("StartButton", std::make_unique<Button>(startButtonParam));
+	}
+	else
+	{
+		startButton->SetActive(true);
+	}
 
-	Button::ConstructorParam quitButtonParam{
-		uiUpdateOrder_,
-		true,
-		DirectX::XMFLOAT2(0.0f, -0.6f),
-		200.0f,
-		200.0f,
-		"Quit",
-		1.0f,
-		0.5f,
-		0.95f,
-		[&]() {
-		}
-	};
+	Button* quitButton = reinterpret_cast<Button*>(WorldManager::Get().GetGameObject("QuitButton"));
+	if (!quitButton)
+	{
+		Button::ConstructorParam quitButtonParam{
+			uiUpdateOrder_,
+			true,
+			DirectX::XMFLOAT2(0.0f, -0.6f),
+			200.0f,
+			200.0f,
+			"Quit",
+			1.0f,
+			0.5f,
+			0.95f,
+			[&]() {
+				if (quitEvent_)
+				{
+					quitEvent_();
+				}
+			}
+		};
 
-	WorldManager::Get().AddGameObject("QuitButton", std::make_unique<Button>(quitButtonParam));
+		WorldManager::Get().AddGameObject("QuitButton", std::make_unique<Button>(quitButtonParam));
+	}
+	else
+	{
+		quitButton->SetActive(true);
+	}
 }
 
 void StartScene::Leave()
 {
-	SetActive(false);
+	std::array<std::string, 4> signatures = {
+		"TitleLabel",
+		"StartButton",
+		"QuitButton",
+		"StartScene",
+	};
+
+	for (const auto& signature : signatures)
+	{
+		GameObject* object = WorldManager::Get().GetGameObject(signature);
+		object->SetActive(false);
+	}
+
+	nextScene_->SetActive(true);
+	nextScene_->Entry();
 }
