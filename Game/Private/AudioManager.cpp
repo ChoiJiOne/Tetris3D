c96f4e9b3d4741569bd2082eba946ceb506bd55a
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-15 11:59:48 +0900

    [DEV] 사운드 리소스를 생성하고, 사운드 ID에 맞게 사운드를 제어하는 기능 추가

--- Game/Private/AudioManager.cpp
+++ Game/Private/AudioManager.cpp
@@ -46,6 +46,90 @@ void AudioManager::Cleanup()
 	bIsSetup_ = false;
 }
 
+int32_t AudioManager::CreateSound(const std::string& path)
+{
+	std::unique_ptr<ma_sound> sound = std::make_unique<ma_sound>();
+
+	if (ma_sound_init_from_file(miniAudioEngine.get(), path.c_str(), 0, nullptr, nullptr, sound.get()) != MA_SUCCESS)
+	{
+		return -1;
+	}
+
+	sounds.push_back(std::move(sound));
+	return countSoundResource_++;
+}
+
+void AudioManager::SetSoundVolume(int32_t soundID, float volume)
+{
+	if (soundID == -1) return;
+
+	volume = std::clamp<float>(volume, 0.0f, 1.0f);
+	ma_sound_set_volume(sounds[soundID].get(), volume);
+}
+
+float AudioManager::GetSoundVolume(int32_t soundID)
+{
+	if (soundID == -1) return 0.0f;
+
+	return ma_sound_get_volume(sounds[soundID].get());
+}
+
+void AudioManager::SetSoundLooping(int32_t soundID, bool bIsLoop)
+{
+	if (soundID == -1) return;
+
+	ma_sound_set_looping(sounds[soundID].get(), static_cast<ma_bool32>(bIsLoop));
+}
+
+bool AudioManager::IsSoundLooping(int32_t soundID)
+{
+	if (soundID == -1) return false;
+
+	return ma_sound_is_looping(sounds[soundID].get()) == MA_TRUE;
+}
+
+void AudioManager::PlaySound(int32_t soundID)
+{
+	if (soundID == -1) return;
+
+	ma_sound_start(sounds[soundID].get());
+}
+
+bool AudioManager::IsPlayingSound(int32_t soundID)
+{
+	if (soundID == -1) return false;
+
+	return ma_sound_is_playing(sounds[soundID].get()) == MA_TRUE;
+}
+
+bool AudioManager::IsDoneSound(int32_t soundID)
+{
+	if (soundID == -1) return false;
+
+	return ma_sound_at_end(sounds[soundID].get()) == MA_TRUE;
+}
+
+void AudioManager::StopSound(int32_t soundID)
+{
+	if (soundID == -1) return;
+
+	ma_sound_stop(sounds[soundID].get());
+}
+
+bool AudioManager::IsStoppingSound(int32_t soundID)
+{
+	if (soundID == -1) return false;
+
+	return !IsDoneSound(soundID) && !IsPlayingSound(soundID);
+}
+
+void AudioManager::ResetSound(int32_t soundID)
+{
+	if (soundID == -1) return;
+
+	ma_sound_seek_to_pcm_frame(sounds[soundID].get(), 0);
+}
+
 AudioManager::AudioManager()
 {
 }
--- Game/Public/AudioManager.h
+++ Game/Public/AudioManager.h
@@ -45,6 +45,110 @@ public:
 	void Cleanup();
 
 
+	/**
+	 * @brief 사운드 리소스를 생성합니다.
+	 *
+	 * @param path 사운드 리소스의 경로입니다.
+	 *
+	 * @return 생성된 사운드 리소스의 아이디를 반환합니다. 생성에 실패하면 -1을 반환합니다.
+	 */
+	int32_t CreateSound(const std::string& path);
+
+
+	/**
+	 * @brief 사운드의 크기를 설정합니다.
+	 *
+	 * @param soundID 크기를 설정할 사운드의 아이디입니다.
+	 * @param volume 사운드의 크기입니다. 범위는 0.0 ~ 1.0 입니다.
+	 */
+	void SetSoundVolume(int32_t soundID, float volume);
+
+
+	/**
+	 * @brief 현재 사운드의 크기를 얻습니다.
+	 *
+	 * @param soundID 소리 크기를 얻을 사운드의 아이디입니다.
+	 *
+	 * @return 현재 사운드의 크기를 얻습니다.
+	 */
+	float GetSoundVolume(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 설정합니다.
+	 *
+	 * @param soundID 반복 여부를 설정할 사운드의 아이디입니다.
+	 * @param bIsLoop 사운드 반복 여부입니다.
+	 */
+	void SetSoundLooping(int32_t soundID, bool bIsLoop);
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 얻습니다.
+	 *
+	 * @param soundID 반복 여부를 확인할 사운드의 아이디입니다.
+	 *
+	 * @return 사운드가 반복한다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsSoundLooping(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드를 플레이합니다.
+	 *
+	 * @note 이전에 중지한 적이 있다면 중지한 시점부터 플레이됩니다.
+	 *
+	 * @param soundID 플레이할 사운드의 아이디입니다.
+	 */
+	void PlaySound(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드가 플레이중인지 확인합니다.
+	 *
+	 * @param soundID 플레이 중인지 확인할 사운드 아이디입니다.
+	 *
+	 * @return 사운드가 플레이 중이라면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsPlayingSound(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드 플레이가 끝났는지 확인합니다.
+	 *
+	 * @param soundID 플레이가 끝났는지 확인할 사운드 아이디입니다.
+	 *
+	 * @return 사운드 플레이가 끝났다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsDoneSound(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드 플레이를 중지합니다.
+	 *
+	 * @param soundID 플레이를 중지할 사운드 아이디입니다.
+	 */
+	void StopSound(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드 플레이가 중지 되었는지 확인합니다.
+	 *
+	 * @param soundID 플레이가 중지 되었는지 확인할 사운드 아이디입니다.
+	 *
+	 * @return 사운드 플레이가 중지 되었다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsStoppingSound(int32_t soundID);
+
+
+	/**
+	 * @brief 사운드를 초기화합니다.
+	 *
+	 * @param soundID 초기화할 사운드의 아이디입니다.
+	 */
+	void ResetSound(int32_t soundID);
+
+
 private:
 	/**
 	 * @brief 게임 내 오디오를 관리하는 클래스의 생성자입니다.
