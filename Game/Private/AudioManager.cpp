0890af63cba7fc3a7196dd3b384074ea69fd1204
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-15 11:50:57 +0900

    [DEV] miniaudio의 엔진을 초기화/해제하는 기능 추가

--- Game/Private/AudioManager.cpp
+++ Game/Private/AudioManager.cpp
@@ -8,10 +8,24 @@
 #include <miniaudio/miniaudio.h>
 // @third party code - END
 
+/**
+ * @brief miniaudio 엔진입니다.
+ */
+static std::unique_ptr<ma_engine> miniAudioEngine = nullptr;
+
+/**
+ * @brief 사운드 리소스 오브젝트입니다.
+ */
+static std::vector<std::unique_ptr<ma_sound>> sounds;
+
 void AudioManager::Setup()
 {
 	if (bIsSetup_) return;
 
+	miniAudioEngine = std::make_unique<ma_engine>();
+	CHECK((ma_engine_init(nullptr, miniAudioEngine.get()) == MA_SUCCESS), "failed to initialize miniaudio...");
+
+	sounds.resize(0);
 	bIsSetup_ = true;
 }
 
@@ -19,6 +33,15 @@ void AudioManager::Cleanup()
 {
 	if (!bIsSetup_) return;
 
+	for (auto& sound : sounds)
+	{
+		ma_sound_uninit(sound.get());
+		sound.reset();
+	}
+
+	ma_engine_uninit(miniAudioEngine.get());
+	miniAudioEngine.reset();
+	
 	countSoundResource_ = 0;
 	bIsSetup_ = false;
 }
