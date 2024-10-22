1ff547be713ba402f74f71191207ffb4a93de92c
choijione <choijione02@gmail.com>
2024-10-22 17:27:18 +0900

    메인 함수에서 게임 앱을 생성/실행/종료를 수행

--- Tetris3D/Src/Main.cpp
+++ Tetris3D/Src/Main.cpp
@@ -5,10 +5,17 @@
 #include <crtdbg.h>
 #endif
 
+#include "GameApp.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+	std::unique_ptr<IApp> app = std::make_unique<GameApp>();
+	app->Startup();
+	app->Run();
+	app->Shutdown();
 	return 0;
 }
