770f69108e132e3dd7e3f527fb69140dcd818e7a
choijione <choijione02@gmail.com>
2024-10-23 08:05:05 +0900

    3D 오일러 카메라 엔티티 추가 및 좌표축을 렌더링하는 코드 추가

--- Tetris3D/Inc/GameScene.h
+++ Tetris3D/Inc/GameScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "IGameScene3D.h"
+#include "DebugDrawManager3D.h"
 
 class GameScene : public IGameScene3D
 {
@@ -13,5 +14,13 @@ public:
 	virtual void Render() override;
 
 private:
+	DebugDrawManager3D* debugDrawMgr_ = nullptr;
 
+	float minX_ = -50.0f;
+	float maxX_ = +50.0f;
+	float minY_ = -50.0f;
+	float maxY_ = +50.0f;
+	float minZ_ = -50.0f;
+	float maxZ_ = +50.0f;
+	float stride_ = 1.0f;
 };
--- Tetris3D/Src/GameScene.cpp
+++ Tetris3D/Src/GameScene.cpp
@@ -1,4 +1,5 @@
 #include "Assertion.h"
+#include "DebugDrawManager3D.h"
 #include "FreeEulerCamera3D.h"
 #include "EntityManager.h"
 #include "GameUtils.h"
@@ -12,7 +13,10 @@
 
 GameScene::GameScene()
 {
+	debugDrawMgr_ = DebugDrawManager3D::GetPtr();
+	mainCamera_ = EntityManager::GetRef().Create<FreeEulerCamera3D>(Vec3f(3.0f, 3.0f, 3.0f), -GameMath::ToRadian(135.0f), -PI / 6.0f, PI_DIV_4, 0.01f, 100.0f);
 
+	updateEntities_.insert({ "MainCamera", mainCamera_,});
 }
 
 GameScene::~GameScene()
@@ -22,5 +26,25 @@ GameScene::~GameScene()
 void GameScene::Render()
 {
 	renderStateMgr_->BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+	debugDrawMgr_->Begin(mainCamera_);
+	{
+		Vec4f color;
+		for (float x = minX_; x <= maxX_; x += stride_)
+		{
+			color = (x == 0.0f) ? Vec4f(0.0f, 0.0f, 1.0f, 1.0f) : Vec4f(0.5f, 0.5f, 0.5f, 0.5f);
+			debugDrawMgr_->DrawLine(Vec3f(x, 0.0f, minZ_), Vec3f(x, 0.0f, maxZ_), color);
+		}
+
+		for (float z = minZ_; z <= maxZ_; z += stride_)
+		{
+			color = (z == 0.0f) ? Vec4f(1.0f, 0.0f, 0.0f, 1.0f) : Vec4f(0.5f, 0.5f, 0.5f, 0.5f);
+			debugDrawMgr_->DrawLine(Vec3f(minX_, 0.0f, z), Vec3f(maxX_, 0.0f, z), color);
+		}
+
+		debugDrawMgr_->DrawLine(Vec3f(0.0f, minY_, 0.0f), Vec3f(0.0f, maxY_, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
+	}
+	debugDrawMgr_->End();
+
 	renderStateMgr_->EndFrame();
 }
